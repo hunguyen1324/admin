715e846670d415dcfe176e65e2752ce60cf2c971
Hai Ton That <haitt.bkdn@gmail.com>
2023-12-19 19:38:37 +0700

    ok

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,114 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Chart from "react-apexcharts";
 import ReactApexChart from 'react-apexcharts';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FetchAll } from '../../Services/DashboardService';
+
 function Dashboard() {
   const [data, setData] = useState([]);
-  const [listdashboard, setListdashboard] = useState([]);
-  const [originalListdashboard, setOriginalListdashboard] = useState([]);
 
   useEffect(() => {
-    getdashboard();
-    // Đặt một khoảng thời gian để gọi lại getCustomers sau mỗi  (3000ms)
-    const intervalId = setInterval(getdashboard, 3000);
+    const getdashboard = async () => {
+      try {
+        const res = await FetchAll();
+        const resData = res.data;
 
-    // Trong useEffect, chúng ta cần trả về một hàm để xử lý khi component unmount
+        const transformedData = resData.map(item => ({
+          label: item.label,
+          men_price: item.men_price,
+          women_price: item.women_price,
+          total: item.men_price + item.women_price,
+          men_product: item.men_product,
+          women_product: item.women_product,
+          total_product: item.men_product + item.women_product
+        }));
+
+        setData(transformedData);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    getdashboard();
+    const intervalId = setInterval(getdashboard, 3000);
     return () => clearInterval(intervalId);
   }, []);
 
-  const getdashboard = async () => {
-    try {
-      const res = await FetchAll();
-      const resData = res.data;
+  const totalProducts = useMemo(() => {
+    return data.reduce((acc, item) => ({
+      men_product: acc.men_product + item.men_product,
+      women_product: acc.women_product + item.women_product
+    }), { men_product: 0, women_product: 0 });
+  }, [data]);
 
-      setListdashboard(resData);
-      setOriginalListdashboard(resData);
-      setData(resData.map(item => ({
-        label: item.label,
-        men_price: item.men_price,
-        women_price: item.women_price,
-        total: item.men_price + item.women_price,
-        men_product: item.men_product,
-        women_product: item.women_product,
-        total_product: item.men_product + item.women_product
-      })));
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-    }
-  };
+  const totalPrices = useMemo(() => {
+    return data.reduce((acc, item) => ({
+      men_price: acc.men_price + item.men_price,
+      women_price: acc.women_price + item.women_price
+    }), { men_price: 0, women_price: 0 });
+  }, [data]);
 
-  const totalmenProduct = data.reduce((acc, item) => acc + item.men_product, 0);
-  const totalwomenProduct = data.reduce((acc, item) => acc + item.women_product, 0);
-  const totalmenprice = data.reduce((acc, item) => acc + item.men_price, 0);
-  const totalwomenprice = data.reduce((acc, item) => acc + item.women_price, 0);
-
-  const chartData_product = {
+  const chartDataProduct = {
     options: {
-      chart: {
-        type: 'pie',
-      },
+      chart: { type: 'pie' },
       labels: ['Men Product', 'Women Product'],
     },
-    series: [totalmenProduct, totalwomenProduct],
+    series: [totalProducts.men_product, totalProducts.women_product],
   };
 
-  const chartData_price = {
+  const chartDataPrice = {
     options: {
-      chart: {
-        type: 'pie',
-      },
-      labels: ['Men price', 'Women price'],
+      chart: { type: 'pie' },
+      labels: ['Men Price', 'Women Price'],
     },
-    series: [totalmenprice, totalwomenprice],
+    series: [totalPrices.men_price, totalPrices.women_price],
+  };
+
+  const barChartData = {
+    options: {
+      chart: { id: "basic-bar" },
+      xaxis: { categories: data.map(item => item.label) },
+    },
+    series: [
+      {
+        name: "Total Price",
+        data: data.map(item => item.total),
+      },
+      {
+        name: "Total Quantity",
+        data: data.map(item => item.total_product),
+      },
+    ]
   };
 
   return (
     <div className="main">
       <Container>
         <div className="App_value">
-          <h1>
-            Chart of clothes sold in a year by label{" "}
-            <i className="fas fa-user"></i>{" "}
-          </h1>
+          <h1>Chart of clothes sold in a year by label <i className="fas fa-user"></i></h1>
           <Row>
             <Col className="col-8">
-              <div className="row">
-                <div className="col-12">
-                  <Chart
-                    options={{
-                      chart: {
-                        id: "basic-bar",
-                      },
-                      xaxis: {
-                        categories: data.map(item => item.label),
-                      },
-                    }}
-                    series={[
-                      {
-                        name: "total price",
-                        data: data.map(item => item.total),
-                      },
-                      {
-                        name: "total quantity",
-                        data: data.map(item => item.total_product),
-                      },
-                    ]}
-                    type="bar"
-                    width="100%"
-                  />
-                </div>
-              </div>
+              <Chart options={barChartData.options} series={barChartData.series} type="bar" width="100%" />
             </Col>
             <Col className="col-4">
-              <div>
-                <div>
-                  <ReactApexChart options={chartData_product.options} series={chartData_product.series} type="pie" width="380" />
-                </div>
-                <div>
-                  <ReactApexChart options={chartData_price.options} series={chartData_price.series} type="pie" width="380" />
-                </div>
-              </div>
+              <ReactApexChart options={chartDataProduct.options} series={chartDataProduct.series} type="pie" width="380" />
+              <ReactApexChart options={chartDataPrice.options} series={chartDataPrice.series} type="pie" width="380" />
             </Col>
           </Row>
         </div>
